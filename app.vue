c1bed2bd4f3d729f25fc1c1bd6be65c2a2677b1a
lapinRagnar <lapinragnar@gmail.com>
2022-09-15 16:50:58 +0200

    2 - ajout pages index

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtLayout>
+
+      <NuxtPage>
+        
+      </NuxtPage>
+      
+    </NuxtLayout>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    Bonjour du template
+</template>
+
+
